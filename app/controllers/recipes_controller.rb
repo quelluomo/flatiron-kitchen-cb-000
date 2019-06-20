5eb92d14ca6bfa54e2c21389cff5f76f4e45b711
Matteo Muscas <thatman.j@gmail.com>
2019-06-20 10:56:14 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -37,7 +37,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, ingredients_attributes:[])
+    params.require(:recipe).permit(:name, ingredient_ids:[])
   end
 
 end
